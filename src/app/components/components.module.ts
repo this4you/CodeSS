d6500986e715a494dd1cff302c1c1ef079245f0e
this4you <me_vlad@ukr.net>
2021-10-28 01:33:32 +0300

    start login form

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
+import { MaterialComponentModuleModule } from '../material-component-module/material-component-module.module';
 
 
 
@@ -11,7 +12,8 @@ import { RegisterFormComponent } from './register-form/register-form.component';
     RegisterFormComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialComponentModuleModule
   ],
   exports: []
 })
--- src/app/material-component-module/material-component-module.module.ts
+++ src/app/material-component-module/material-component-module.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule
   ],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class MaterialComponentModuleModule { }
