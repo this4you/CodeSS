8caa0e59a6a60fc8b8bc05d940f98917d6ff9199
this4you <me_vlad@ukr.net>
2021-12-23 23:18:51 +0200

    code-editor

--- src/app/components/code-editor/code-editor.component.ts
+++ src/app/components/code-editor/code-editor.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'code-ss-code-editor',
   templateUrl: './code-editor.component.html',
-  styleUrls: ['./code-editor.component.scss']
+  styleUrls: ['./code-editor.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CodeEditorComponent implements OnInit {
-
+  public codeText: string;
   constructor() { }
 
   ngOnInit(): void {
