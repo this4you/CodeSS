3d7f9c0bb39410f5ccffccf2ba57d00f076ee9d5
V.Mertvichenko <v.mertvichenko@tsintegr.com>
2022-02-06 18:46:14 +0200

    useful links

--- src/app/material-component-module/material-component-module.module.ts
+++ src/app/material-component-module/material-component-module.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatIconModule} from '@angular/material/icon';
-import { MatSnackBarModule} from "@angular/material/snack-bar";
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatOptionModule } from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import {MatSelectModule} from '@angular/material/select';
     MatCardModule,
     MatDividerModule,
     MatOptionModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTableModule  
   ]
 })
 export class MaterialComponentModuleModule { }
--- src/app/pages/main-page/subpages/useful-links-subpage/useful-links-subpage.component.ts
+++ src/app/pages/main-page/subpages/useful-links-subpage/useful-links-subpage.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+export type LinkModel = {
+  Name: string;
+  Description: string;
+  Url: string;
+};
+
+const ELEMENT_DATA: LinkModel[] = [
+  {Name: "Test", Description: "Description", Url: "http://example.com"},
+  {Name: "Test", Description: "Description", Url: "http://example.com"},
+  {Name: "Test", Description: "Description", Url: "http://example.com"},
+  {Name: "Test", Description: "Description", Url: "http://example.com"},
+  {Name: "Test", Description: "Description", Url: "http://example.com"},
+  {Name: "Test", Description: "Description", Url: "http://example.com"},
+  {Name: "Test", Description: "Description", Url: "http://example.com"},
+];
+
+
 @Component({
   selector: 'code-ss-useful-links-subpage',
   templateUrl: './useful-links-subpage.component.html',
   styleUrls: ['./useful-links-subpage.component.scss']
 })
 export class UsefulLinksSubpageComponent implements OnInit {
-
+  displayedColumns: string[] = ['Name', 'Url', 'Description'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit(): void {
